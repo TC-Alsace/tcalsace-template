0fd587d34f7e0884e77441ab468261473b176207
Pierre Rudloff <contact@rudloff.pro>
2014-11-22 01:18:06 +0100

    Prochaine date : Débat sur la vie nocturne

--- bloc-presentation.php
+++ bloc-presentation.php
@@ -1,16 +1,15 @@
 <section id="bloc-presentation">
-	<div id="kesako">
-		<h2 class="lettrine">TC Alsace, késako ?</h2>
-		<p>&Agrave; l’heure où les citoyens tendent à montrer a minima du scepticisme, au pire du dégout, vis-à-vis du débat politique partisan et de la conduite des affaires publiques, nous, membres de TC&nbsp;Alsace, sommes convaincus que l’intérêt des citoyens ne pourra qu’être suscité par la démonstration du caractère concret et constructif du débat politique.</p>
-	</div><!--
-	
+    <div id="kesako">
+        <h2 class="lettrine">TC Alsace, késako ?</h2>
+        <p>&Agrave; l’heure où les citoyens tendent à montrer a minima du scepticisme, au pire du dégout, vis-à-vis du débat politique partisan et de la conduite des affaires publiques, nous, membres de TC&nbsp;Alsace, sommes convaincus que l’intérêt des citoyens ne pourra qu’être suscité par la démonstration du caractère concret et constructif du débat politique.</p>
+    </div><!--
  --><div id="next">
-		<h2 class="lettrine">Prochaines dates</h2>
-		<ul class="next-date">
-			<li><time datetime="2014-10-26 11:00"><span>26</span><span>oct</span></time></li>
-			<li>Conseil d'administration</li>
-			<li>Lieu à définir</li>
-			<li>11h00</li>
-		</ul>
-	</div>
+        <h2 class="lettrine">Prochaines dates</h2>
+        <ul class="next-date">
+            <li><time datetime="2014-11-24 17:30"><span>24</span><span>nov</span></time></li>
+            <li>Débat sur la vie nocturne</li>
+            <li>FEC, 17 place Saint-Etienne à Strasbourg</li>
+            <li>17h30</li>
+        </ul>
+    </div>
 </section>
