a0cd5900975cf6c2ab457f3b94ec547ac8040a25
Pierre Rudloff <contact@rudloff.pro>
2016-09-08 23:40:13 +0000

    Applied fixes from StyleCI

--- content.php
+++ content.php
@@ -7,47 +7,47 @@
 				<aside>
 					<span class="auteur"><?php the_author_posts_link(); ?></span>
 					<span class="date"><?php the_date(); ?></span>
-					<?php if ( !is_page() ) : ?><span class="categories"><?php the_category(); ?></span><?php endif; ?>
+					<?php if (!is_page()) : ?><span class="categories"><?php the_category(); ?></span><?php endif; ?>
 				</aside>
 			</header>
 			<div class="legende-thumbnail"><?php echo get_post_field('post_excerpt', get_post_thumbnail_id()); ?></div>
 		</div>
 		
 		<?php
-			// On regarde s'il existe un message d'information et on l'affiche
-	
-			// Le contenu est-il archivé ?
-			if (get_post_meta(get_the_ID(), 'archive', true) && is_single()) :
-			
-				get_template_part('contenu-archive');
-			
-			else :
-				// Le contenu est-il partisan ?
-				if (get_post_meta(get_the_ID(), 'partisan', true) && is_single()) :
-				
-					get_template_part('contenu-partisan');
-				
-				// Le contenu est-il alors juste âgé ?
-				else :
-				
-					// On calcule le nombre de secondes entre maintenant et la date de l'article
-					$date['secParJours'] = 60*60*24;
-					$date['article'] = strtotime(get_the_date('Y-m-d'));
-					$date['now'] = time();
-					$date['diff'] = $date['now'] - $date['article'];
-					$date['jours'] = round($date['diff'] / $date['secParJours']);
-					$date['annees'] = floor($date['jours'] / 365);
-					
-					if ($date['annees'] >= 1 && is_single()	) :
-					
-						get_template_part('contenu-vieux');
-					
-					endif;
-					
-				endif;
-			
-			endif;
-		?>
+            // On regarde s'il existe un message d'information et on l'affiche
+
+            // Le contenu est-il archivé ?
+            if (get_post_meta(get_the_ID(), 'archive', true) && is_single()) :
+
+                get_template_part('contenu-archive');
+
+            else :
+                // Le contenu est-il partisan ?
+                if (get_post_meta(get_the_ID(), 'partisan', true) && is_single()) :
+
+                    get_template_part('contenu-partisan');
+
+                // Le contenu est-il alors juste âgé ?
+                else :
+
+                    // On calcule le nombre de secondes entre maintenant et la date de l'article
+                    $date['secParJours'] = 60 * 60 * 24;
+                    $date['article'] = strtotime(get_the_date('Y-m-d'));
+                    $date['now'] = time();
+                    $date['diff'] = $date['now'] - $date['article'];
+                    $date['jours'] = round($date['diff'] / $date['secParJours']);
+                    $date['annees'] = floor($date['jours'] / 365);
+
+                    if ($date['annees'] >= 1 && is_single()) :
+
+                        get_template_part('contenu-vieux');
+
+                    endif;
+
+                endif;
+
+            endif;
+        ?>
 	
 		<div class="contenu-principal"><?php echo the_content(); ?></div>
 	
--- page.php
+++ page.php
@@ -4,17 +4,17 @@ ini_set('error_reporting', E_ALL); ?>
 
 	<div id="single">
 		<?php 
-			// Start the Loop.
-			while ( have_posts() ) : the_post();
-				/*
-				 * Include the post format-specific template for the content. If you want to
-				 * use this in a child theme, then include a file called called content-___.php
-				 * (where ___ is the post format) and that will be used instead.
-				 */
-				get_template_part( 'content' );
-				
-			endwhile;
-		?>
+            // Start the Loop.
+            while (have_posts()) : the_post();
+                /*
+                 * Include the post format-specific template for the content. If you want to
+                 * use this in a child theme, then include a file called called content-___.php
+                 * (where ___ is the post format) and that will be used instead.
+                 */
+                get_template_part('content');
+
+            endwhile;
+        ?>
 	</div>
 
 <?php get_footer(); ?>
